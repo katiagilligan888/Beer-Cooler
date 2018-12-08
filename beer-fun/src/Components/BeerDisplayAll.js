1791bf77a9f480111595d97f1322adf6edc47e57
katiagilligan888 <katia.gilligan88@gmail.com>
2018-12-07 17:16:38 -0700

    Added new beer form and post request working

--- beer-fun/src/Components/BeerDisplayAll.js
+++ beer-fun/src/Components/BeerDisplayAll.js
@@ -7,7 +7,8 @@ class BeerDisplayAll extends Component {
   constructor() {
     super();
     this.state = {
-      beer: []
+      beer: [],
+      beerName: ""
     };
   }
 
@@ -18,16 +19,61 @@ class BeerDisplayAll extends Component {
       )
       .then(beers => {
         this.setState({ beer: beers.data });
-        console.log(beers);
       });
   }
+
+  onInputChangeHandler = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  onSubmitHandler = event => {
+    event.preventDefault();
+    const newBeer = {
+      name: this.state.beerName,
+      likes: "0"
+    };
+
+    axios
+      .post(
+        "https://cors-anywhere.herokuapp.com/https://beer.fluentcloud.com/v1/beer",
+        newBeer
+      )
+      .then(id => {
+        axios
+          .get(
+            "https://cors-anywhere.herokuapp.com/https://beer.fluentcloud.com/v1/beer"
+          )
+          .then(beers => {
+            this.setState({ beer: beers.data, beerName: "" });
+          });
+      });
+
+    // since the post request does not return the updated list of beers,
+    // a new get request has to be made to the API to update the UI without refreshing the page!
+  };
 
   render() {
     return (
       <div className="beer-display">
         <Header />
+        <h3>Have an IPA you want to add to the API?</h3>
+        <p>Add a new beer here!</p>
+        <form onSubmit={this.onSubmitHandler}>
+          <input
+            value={this.state.beerName}
+            name="beerName"
+            onChange={this.onInputChangeHandler}
+            type="text"
+            placeholder="Name of Beer"
+          />
+          <button className="beer-form-button" type="submit">
+            Add it Now!
+          </button>
+        </form>
         {this.state.beer.map(beer => {
-          return <Beer beer={beer} />;
+          return <Beer key={beer.id} beer={beer} />;
         })}
       </div>
     );
