ca9d055dd533ea3225adec491d9267fc1de17668
katiagilligan888 <katia.gilligan88@gmail.com>
2018-12-07 17:48:57 -0700

    Header logo created

--- beer-fun/src/Components/Header.js
+++ beer-fun/src/Components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+import BeerLogo from '../beer-cooler-logo.jpg'; 
 
 const Header = props => {
   return (
     <div className="header">
       <h1>Beer Cooler</h1>
       <h2> A Place to Categorize Your Favorite Beers </h2>
+      <img className = "logo" src ={BeerLogo} width = "400" height = "300"/>
     </div>
   );
 };
